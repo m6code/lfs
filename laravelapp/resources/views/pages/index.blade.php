34842bd619eb5078f76b21bfed74573b5c1aed7e
Benjamin M6 <benjamynfrank101@gmail.com>
2018-04-08 20:35:24 +0100

    remove repeating/recurring code

--- laravelapp/resources/views/pages/index.blade.php
+++ laravelapp/resources/views/pages/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
+@section('content')
     <h1>Homepage</h1>
     <p>Welcome to the homepage</p>
-    </body>
-</html>
+@endsection
--- laravelapp/resources/views/pages/services.blade.php
+++ laravelapp/resources/views/pages/services.blade.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Services</title>
-    </head>
-    <body>
+@extends('layouts.app')
+@section('content')
     <h1>Services</h1>
     <p>Our services goes here</p>
-    </body>
-</html>
+@endsection
