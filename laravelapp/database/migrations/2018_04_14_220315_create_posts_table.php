1ebcc3908fb7aede369cdf2deb098232acabbd4c
m6code <benjamynfrank101@gmail.com>
2018-04-20 22:01:54 +0100

    fix error when creating data for the post table

--- laravelapp/database/migrations/2018_04_14_220315_create_posts_table.php
+++ laravelapp/database/migrations/2018_04_14_220315_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('posts');
+            $table->string('title');
             $table->mediumText('body');
             $table->timestamps();
         });
